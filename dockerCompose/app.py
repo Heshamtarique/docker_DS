863eaad6423e5aa9831d04aac1c68116bfd22ba0
Hesham Mohammad Tarique <heshamtarique.jmi@gmail.com>
2024-06-03 00:28:29 +0530

    docker compose service

---
+++ dockerCompose/app.py
@@ -0,0 +1,28 @@
+from flask import Flask
+import os
+import redis # in memory caching
+import time
+
+import redis.exceptions
+
+
+app = Flask(__name__)
+
+cache = redis.Redis(host='redis', port = 6379)
+
+def get_hit_count():
+    retries = 5
+    while True:
+        try:
+            # cache.reset_retry_count()
+            return cache.incr('hits')
+        except redis.exceptions.ConnectionError as exc:
+            if retries==0:
+                raise exc
+            retries-=1
+            time.sleep(0.5)
+            
+@app.route('/')
+def hello():
+    count = get_hit_count()
+    return ' hesham i have seen {} times. \n'.format(count)
